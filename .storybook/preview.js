377e32ea22863c4b085faaf2574a30cc53a06b55
ilumin <iolumin@gmail.com>
2021-03-17 13:07:28 +0700

    chore: add Layout into storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
+import Layout from '../components/Layout'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: { expanded: true },
-  layout: 'centered',
+  // layout: 'centered',
 }
 
-export const decorator = [
-  (story) => <div style={{ padding: '1em' }}>{story()}</div>,
+export const decorators = [
+  (story) => (
+    <Layout>
+      <div style={{ padding: '1em' }}>{story()}</div>
+    </Layout>
+  ),
 ]
