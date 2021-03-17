90901141e887adec92d0fa9d2a1938531789616e
ilumin <iolumin@gmail.com>
2021-03-17 16:54:37 +0700

    feat: add `FormField`

---
+++ components/Form/FormField.stories.jsx
@@ -0,0 +1,25 @@
+import { Simple as SimpleSelect } from '../Select/Select.stories'
+import FormField from './FormField'
+
+export default {
+  title: 'Components/FormField',
+  component: FormField,
+}
+
+const Template = (args) => <FormField {...args} />
+
+export const SingleInput = Template.bind({})
+SingleInput.args = {
+  children: <SimpleSelect {...SimpleSelect.args} />,
+  label: 'Color',
+  helpText: 'Pick RGB',
+}
+
+export const SingleInputWithError = Template.bind({})
+SingleInputWithError.args = {
+  children: <SimpleSelect {...SimpleSelect.args} />,
+  label: 'Color',
+  helpText: 'Pick RGB',
+  required: true,
+  error: 'Please select color.',
+}
