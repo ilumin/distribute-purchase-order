252e0003c104e0f5fbedaac16ffc9c42694704ac
ilumin <iolumin@gmail.com>
2021-03-19 12:14:53 +0700

    feat: add `cartSlice`

---
+++ reducers/cartSlice.js
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const submitCart = createAsyncThunk(
+  'cart/submit',
+  async (requestBody) => {
+    try {
+      const { data } = await axios.post('/api/cart', requestBody)
+      return data
+    } catch (error) {
+      console.error(error)
+      return error
+    }
+  }
+)
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    total_qty: 0,
+    total_price: 0,
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const { product, location } = action.payload
+      const item = {
+        id: product.id,
+        name: product.name,
+        unit_price: product.unit_price,
+        qty: location.qty,
+        fee: location.fee,
+        total_price: product.unit_price * location.qty + location.fee,
+      }
+
+      state.items.push(item)
+      state.total_qty += item.qty
+      state.total_price += item.total_price
+    },
+    removeItem: (state, action) => {
+      const { id } = action.payload
+      const removedItem = state.items.find((item) => item.id === id)
+      const remainingItems = state.items.filter((item) => item.id !== id)
+
+      state.items = remainingItems
+      state.total_qty -= removedItem.qty
+      state.total_price -= removedItem.total_price
+    },
+    updateItem: (state, action) => {
+      const { id, qty } = action.payload
+      const updatedItems = state.items.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            total_price: item.unit_price * qty + item.fee,
+            qty,
+          }
+        }
+
+        return item
+      })
+
+      state.items = updatedItems
+      state.total_qty = updatedItems.reduce((acc, cur) => {
+        acc += cur.qty
+        return acc
+      }, 0)
+      state.total_price = updatedItems.reduce((acc, cur) => {
+        acc += cur.total_price
+        return acc
+      }, 0)
+    },
+  },
+})
+
+export const { addItem, removeItem, updateItem } = cartSlice.actions
+
+export default cartSlice.reducer
