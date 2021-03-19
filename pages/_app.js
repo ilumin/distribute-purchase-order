2fc385fbaff84ef336d88d58ea46e196cc055900
ilumin <iolumin@gmail.com>
2021-03-19 12:12:35 +0700

    chore: add redux provider and empty reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
 import Layout from 'components/Layout'
+import { Provider } from 'react-redux'
+import rootReducer from 'reducers'
+
+const store = configureStore({
+  reducer: rootReducer,
+})
 
 // eslint-disable-next-line
 function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
