9932dbc3b90fa59b8471a24f2eaa3315b9bb754e
ilumin <iolumin@gmail.com>
2021-03-19 12:13:36 +0700

    feat: add `productSlice`

---
+++ reducers/productSlice.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchProducts = createAsyncThunk(
+  'product/fetchProducts',
+  async () => {
+    try {
+      const { data } = await axios.get('/api/products')
+      return data
+    } catch (error) {
+      console.error(error)
+      return error
+    }
+  }
+)
+
+const productSlice = createSlice({
+  name: 'product',
+  initialState: {
+    selectedProduct: {},
+    products: [],
+    loading: true,
+    error: undefined,
+  },
+  reducers: {
+    selectProduct: (state, action) => {
+      state.selectedProduct = state.products.filter(
+        (item) => item.id === action.payload
+      )
+    },
+  },
+  extraReducers: {
+    [fetchProducts.pending]: (state) => {
+      state.loading = true
+    },
+    [fetchProducts.rejected]: (state, action) => {
+      state.loading = false
+      state.error = action.payload
+    },
+    [fetchProducts.fulfilled]: (state, action) => {
+      state.loading = false
+      state.products = action.payload
+    },
+  },
+})
+
+export const { selectProduct } = productSlice.actions
+
+export const productSelector = {
+  availableDates: ({ product }) => product.selectedProduct.available_dates,
+  selectedProduct: ({ product }) => product.selectedProduct,
+  allProducts: ({ product }) => product.products,
+}
+
+export default productSlice.reducer
